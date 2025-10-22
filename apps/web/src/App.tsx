47d76c54c3d1dd357d3cfdb128ae9eac9f27db63
Victor Gabriel <victorgabel99@gmail.com>
2025-10-22 12:36:01 -0300

    chore: setup initial folder structure and config files

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,13 @@
+import "./global.css";
+
+function App() {
+  return (
+    <div>
+      <p className="w-full h-screen bg-zinc-950 text-zinc-200 flex items-center justify-center">
+        opa
+      </p>
+    </div>
+  );
+}
+
+export default App;
